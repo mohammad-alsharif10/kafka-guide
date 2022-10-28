5419dfe1937efe9b43ad4f644acc7a2f5556269a
mohammad alshsrif <mohammad-alsharif10@outlook.com>
2022-10-28 21:58:33 +0200

    init

--- kafka-producer/src/main/java/com/guide/kafkaproducer/config/KafkaConfig.java
+++ kafka-producer/src/main/java/com/guide/kafkaproducer/config/KafkaConfig.java
@@ -4,17 +4,9 @@ package com.guide.kafkaproducer.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.guide.kafkaproducer.util.Constants;
 import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 public class KafkaConfig {
@@ -22,8 +14,8 @@ public class KafkaConfig {
     @Bean
     public NewTopic topic() {
         return TopicBuilder.name(Constants.EMPLOYEE_TOPIC)
-                .partitions(3)
-                .replicas(1)
+                .partitions(6)
+                .replicas(3)
                 .build();
     }
 
@@ -35,30 +27,30 @@ public class KafkaConfig {
 //                .build();
 //    }
 
-    @Bean
-    public ProducerFactory<String, String> producerFactory() {
-        return new DefaultKafkaProducerFactory<>(producerConfigs());
-    }
+//    @Bean
+//    public ProducerFactory<String, String> producerFactory() {
+//        return new DefaultKafkaProducerFactory<>(producerConfigs());
+//    }
 
-    @Bean
-    public Map<String, Object> producerConfigs() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.CLIENT_ID_CONFIG, "THE_PRODUCER_APPLICATION");
-        props.put(ProducerConfig.ACKS_CONFIG, "all");
-        props.put(ProducerConfig.LINGER_MS_CONFIG, 10000);
+//    @Bean
+//    public Map<String, Object> producerConfigs() {
+//        Map<String, Object> props = new HashMap<>();
+//        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "pkc-6ojv2.us-west4.gcp.confluent.cloud:9092");
+//        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//        props.put(ProducerConfig.CLIENT_ID_CONFIG, "THE_PRODUCER_APPLICATION");
+//        props.put(ProducerConfig.ACKS_CONFIG, "all");
+//        props.put(ProducerConfig.LINGER_MS_CONFIG, 10000);
+//
+//        // See https://kafka.apache.org/documentation/#producerconfigs for more properties
+//        return props;
+//    }
 
-        // See https://kafka.apache.org/documentation/#producerconfigs for more properties
-        return props;
-    }
 
-
-    @Bean
-    public KafkaTemplate<String, String> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
-    }
+//    @Bean
+//    public KafkaTemplate<String, String> kafkaTemplate() {
+//        return new KafkaTemplate<>(producerFactory());
+//    }
 
     @Bean
     public ObjectMapper objectMapper() {
